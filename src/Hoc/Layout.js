b68f79649d3ae866e42c355a39f380792c0d5e17
Simon Maher <simon.maher@integrify.io>
2019-06-10 16:52:35 +0300

    Finished footer, added logo and text.

--- src/Hoc/Layout.js
+++ src/Hoc/Layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "../Header_footer/Header"
+import Footer from "../Header_footer/Footer"
 const Layout = (props) => {
     return (
         <div>
             <Header />
             {props.children}
+            <Footer />
         </div>
     )
 }
