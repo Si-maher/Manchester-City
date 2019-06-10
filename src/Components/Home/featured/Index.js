b767f83679d721a5f799329a2ba26cd018505722
Simon Maher <simon.maher@integrify.io>
2019-06-10 19:22:17 +0300

    Created stripe function and animation

--- src/Components/Home/featured/Index.js
+++ src/Components/Home/featured/Index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import Stripes from "./Stripes"
 const Featured = () => {
     return (
         <div className="featured_wrapper">
+            <Stripes />
 
         </div>
     )
