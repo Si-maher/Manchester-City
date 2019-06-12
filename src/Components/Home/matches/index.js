9a7d7b31c56a601286b5a897039ed9d923095ff2
Simon Maher <simon.maher@integrify.io>
2019-06-12 12:54:18 +0300

    Added linkto, style, Matches Home function

--- src/Components/Home/matches/index.js
+++ src/Components/Home/matches/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Tag } from "../../ui/misc"
 
 const MatchesHome = () => {
     return (
         <div className="home_matches_wrapper">
             <div className="container">
-                TAG
+                <Tag
+                    bck='#0e1731'
+                    size='50px'
+                    color='#ffffff'
+
+                >
+                    Matches
+                 </Tag>
                 BLOCK
-                TAG
-        </div>
+                <Tag
+                    bck='#ffffff'
+                    size='22px'
+                    color='#0e1731'
+                    link={true}
+                    linkto='/the_team'
+
+                >
+                    See more matches
+    
+                </Tag>
+            </div>
 
         </div>
     );
